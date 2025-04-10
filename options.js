88e80ece471518c71a592fd4b763578737b6787a
xo-sarthak <xosarthakstan@gmail.com>
2025-04-10 13:34:56 +0530

    adding update shortcut feature

--- options.js
+++ options.js
@@ -2,8 +2,10 @@ console.log("✅ options.js is running!");
 
 let addShortcut = document.getElementById("addShortcut");
 let shortcutList = document.getElementById("shortcutList");
+let updateShortcut = document.getElementById("updateShortcut")
 
 let shortcuts = {};
+let editingKey = null;
 
 chrome.storage.sync.get("shortcuts", (data) => {
     if(data.shortcuts) {
@@ -24,6 +26,29 @@ addShortcut.addEventListener("click", (e) => {
         document.getElementById("expandedKey").value = "";
     }
 });
+
+updateShortcut.addEventListener("click", () => {
+    console.log("☄️")
+    let key = document.getElementById("shortcutKey").value.trim();
+    let value = document.getElementById("expandedKey").value.trim();
+
+    if(key && value && editingKey !== null) {
+        if(editingKey !== key) {
+            delete shortcuts[key];
+        }
+
+        shortcuts[key] = value;
+        saveShortcut();
+        updateList();
+
+        document.getElementById("shortcutKey").value = "";
+        document.getElementById("expandedKey").value = "";
+        editingKey = null;
+
+        document.getElementById("addShortcut").style.display = "inline-block";
+        document.getElementById("updateShortcut").style.display = "none";
+    }
+})
 
 function saveShortcut() {
     chrome.storage.sync.set({ shortcuts }, () => {
@@ -52,8 +77,24 @@ function updateList() {
             delete shortcuts[key];
             deleteShortcut();
             updateList();
-        })
+        });
 
+        let updateBtn = document.createElement("button");
+        updateBtn.style.cursor = "pointer";
+        updateBtn.style.marginLeft = "10px";
+        updateBtn.textContent = "Update";
+        updateBtn.addEventListener("click", () => {
+            console.log("☑️");
+            editingKey = key;
+
+            document.getElementById("shortcutKey").value = key;
+            document.getElementById("expandedKey").value = shortcuts[key];
+
+            document.getElementById("addShortcut").style.display = "none";
+            document.getElementById("updateShortcut").style.display = "inline-block";
+        });
+        
+        item.appendChild(updateBtn);
         item.appendChild(deleteBtn);
         shortcutList.appendChild(item);
     }
