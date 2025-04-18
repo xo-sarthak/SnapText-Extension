cd0194b43889349a0e27e509e0eabb1854032dc8
xo-sarthak <xosarthakstan@gmail.com>
2025-04-18 17:13:42 +0530

    removing hardcoded values in content.js, synced it with chrome.storage

--- content.js
+++ content.js
@@ -3,12 +3,15 @@ document.addEventListener("keydown", function (event) {
 
   if (target.tagName === "TEXTAREA" || target.tagName === "INPUT") {
     if (event.key === " " || event.key === "\n") {
+      chrome.storage.sync.get("shortcuts", function(data) {
+        let shortcuts = data.shortcuts || {};
+      });
       let text = target.value;
-      let shortcuts = {
-        ":addr": "123 Street, City",
-        ":email": "user@example.com",
-        ":ty": "Thank you!",
-      };
+      // let shortcuts = {
+      //   ":addr": "123 Street, City",
+      //   ":email": "user@example.com",
+      //   ":ty": "Thank you!",
+      // };
 
       let words = text.split(" ");
       let lastWord = words[words.length - 2];
