929fce9b58768fedac3de7319656307df43116d9
xo-sarthak <xosarthakstan@gmail.com>
2025-07-11 13:45:14 +0530

    feat: add MutationObserver to support dynamically added input, textarea, and contenteditable elements

--- content.js
+++ content.js
@@ -116,3 +116,46 @@ function processContentEditable(element) {
     selection.addRange(newRange); // adds this new range.
   }
 }
+
+// alright now to add better functionality to the extension.
+// implementing MutationObserver.
+// we need this to make our extension compatible with SPAs(Single Page Applications).
+// currently we can handle elements that were loaded on the page with the initial load, but we cant handle inputs and editors added dynamically after the page load, in order to detect them we would require this.
+// example of SPAs - Notion, Gmail.
+
+function isTargetElement(el) {    // the basic use case of this function is that we will call this fucntion whenever we need to check if the node/element is textarea, input or contenteditable, and it will be returning true or false.
+  return (                        // this dosent work on its own, we use it just to check so we call it when we need and it answers accordingly.
+    el.tagName === "INPUT" ||
+    el.tagName === "TEXTAREA" ||
+    el.isContentEditable
+  ); // basically to detect input, textarea and contenteditable like we did above
+} // this return is a boolean type, wil return true or false
+
+const observer = new MutationObserver((mutationsList) => { // so here this MutationObserver is an object that watches the DOM, and whenever the change is detected like an element is added, it triggers the callback function present inside mutationsList i.e. for list of changes that took place.
+  for(const mutation of mutationsList) {      // this means out of the list provided to it of the changes that is mutations here, take a single mutation, its written here like a enhanced for loop in java.
+    if(mutation.type === "childList") {      // we only want to cater the new elements added so we onky focus on the type "childList".
+      mutation.addedNodes.forEach((node) => {  // for every change/mutation taking place, there can be multiple elements/nodes added for each mutation so we need to sort out the ones we need.
+        if(!(node instanceof HTMLElement)) return;   // If the node is not an HTMLElement, we skip it (like text comments, etc.).
+        
+        if(isTargetElement(node)) {     // we call the function from top that we created to check. this if condition is checking the "parent node".
+          node.addEventListener("keydown", expandShortcuts);   // if it says true that means we found the element/node that concerns us, meaning we will cater it.
+        }
+
+        if(node.nodeType === Node.ELEMENT_NODE && node.querySelectorAll) {    // this if condition is for the children elements under the parent, for cases like if parent isnt a input, textarea or contenteditable but the children are.
+          const fields = node.querySelectorAll("input, textarea, [contenteditable='true']");    // querySelector is required in order to select those 3 things out of the element
+          fields.forEach((el) => {
+            el.addEventListener("keydown", expandShortcuts);
+          });
+        }
+      });
+    }
+  }
+});
+
+observer.observe(document.body, {   // here we are telling observer to keep watching the complete body of the document for changes.
+  childList: true,    // this tells the observer to watch for the nodes that are being added and removed in the given section, in this case the whole body.
+  subtree: true,      // this tells the observer to dive deeper meaning dont just watch document.body but also recursively go deep inside the nested child of the body.
+});
+// We’re telling the observer:
+// “Start watching the entire page for any new elements added anywhere inside it — not just on top-level body but deep down too.”
+
