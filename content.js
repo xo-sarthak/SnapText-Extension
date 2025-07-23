3117c4451ff89bebe3c9377cc7120c437aa83031
xo-sarthak <xosarthakstan@gmail.com>
2025-07-23 08:56:46 +0530

    fix(core): Resolve startup race condition on page load
    Previously, the snapTextEnabled status and the shortcuts data were fetched in two separate, asynchronous chrome.storage.sync.get() calls. This created a race condition where event listeners could be attached before the shortcuts were loaded, causing expansion to fail intermittently.
    
    This commit refactors the startup logic into a single initialization function. It combines both data fetches into one API call, guaranteeing that all data is present before attaching listeners or running other logic. This ensures reliable startup and eliminates the bug.

--- content.js
+++ content.js
@@ -1,39 +1,64 @@
+let snapTextEnabled = true;
+let shortcuts = {};
+
+function initialization () {
+  chrome.storage.sync.get(['shortcuts', 'snapTextEnabled'], (data) => {
+    snapTextEnabled = data.snapTextEnabled ?? true;
+    shortcuts = data.shortcuts || {};
+
+    if(!snapTextEnabled) {
+      console.log("Extension disabled")
+      return;
+    }
+
+    const textFields = document.querySelectorAll("textarea, input, [contenteditable = 'true']");
+    textFields.forEach((field) => {
+      field.addEventListener("keydown", expandShortcuts)
+    })
+  })
+}
+initialization();
+
 // if(!snapTextEnabled) return; // commented out as it can be undefined and null and give false due to them being falsy or not true exactly which will stop the execution of the code due to return, so we by default set it to true;
 // the logic where i check if the extension is enabled or disabled by the user.
-let snapTextEnabled = true;
 
-chrome.storage.sync.get(['snapTextEnabled'], (data) => {
-  snapTextEnabled = data.snapTextEnabled ?? true; // if data.snapTextEnabled is having a value then take it, else return true;
-});
+//DUE TO THE WHOLE INITIALIZATION CODE ABOVE, WE COMMENTED OUT THE CODE BELOW!!!
+// chrome.storage.sync.get(["snapTextEnabled"], (data) => {
+//   snapTextEnabled = data.snapTextEnabled ?? true; // if data.snapTextEnabled is having a value then take it, else return true;
+//   if (!snapTextEnabled) {
+//     console.log("SnapText is disabled");
+//     return;
+//   }
+//   console.log("script loaded");
 
-if(!snapTextEnabled) return; // tomake sure we act upon the value that we are getting from popup.js.
-console.log("script loaded");
+//   loadShortcuts(); // this will cause bug, as loadshortcut will run but code wont wait for all of them to get loaded, causing error.as event listeners of shortcuts are attached instantly so ofc, expandshortcut wont work w/o the shortcut loaded.
 
-let shortcuts = {};
-// console.log("typeof expandShortcuts:", typeof expandShortcuts);
+//   // Attaching expand listener to already existing elements
+//   document
+//     .querySelectorAll("input, textarea, [contenteditable='true']")
+//     .forEach((el) => {
+//       el.addEventListener("keydown", expandShortcuts);
+//     });
+//   document.addEventListener("keydown", (e) => {
+//     console.log("Keydown detected on: ", e.target.tagName, e.target);
+//   });
+// });
 
-// Load shortcuts once at startup
-function loadShortcuts() {
-  chrome.storage.sync.get("shortcuts", function (data) {
-    console.log("shortcurtsloaded from storage", data);
-    shortcuts = data.shortcuts || {};
-  });
-}
+// // if(!snapTextEnabled) return; // to make sure we act upon the value that we are getting from popup.js.
 
-loadShortcuts();
+// // console.log("typeof expandShortcuts:", typeof expandShortcuts);
 
-// Attaching expand listener to already existing elements
-document
-  .querySelectorAll("input, textarea, [contenteditable='true']")
-  .forEach((el) => {
-    el.addEventListener("keydown", expandShortcuts);
-  });
-document.addEventListener("keydown", (e) => {
-  console.log("Keydown detected on: ", e.target.tagName, e.target);
-});
+// // Load shortcuts once at startup
+// function loadShortcuts() {
+//   chrome.storage.sync.get("shortcuts", function (data) {
+//     console.log("shortcurtsloaded from storage", data);
+//     shortcuts = data.shortcuts || {};
+//   });
+// }
 
 // Main expansion logic
 function expandShortcuts(event) {
+  if(!snapTextEnabled) return; // this line makes sure that if my snaptext is wokring then only execute this function, else dont execute expandshortcut
   if (event.key === " " || event.key === "Tab") {
     console.log("Shortcut Trigger Key:", event.key);
 
@@ -59,11 +84,12 @@ function processInputOrTextArea(element) {
   // let words = text.split(" ");
   // let lastWord = words[words.length - 1]; the reason these two lines have been commented out is because we have created a helper function that will do work of these lines.
   // let lastWord = getLastWord(textBeforeCursor); // changing this to line below, sanitize fix!!!
-  const {  original, sanitized } = getLastWord(textBeforeCursor);
+  const { original, sanitized } = getLastWord(textBeforeCursor);
 
-  console.log("Last word detected:", lastWord);
+  console.log("Last word detected:", `'${original}', '${sanitized}'`);
 
-  if (shortcuts[sanitized]) { // changes were made lastWord -> sanitized.
+  if (shortcuts[sanitized]) {
+    // changes were made lastWord -> sanitized.
     // const lastWordStart = textBeforeCursor.length - lastWord.length;
     // const newText =
     //   text.substring(0, lastWordStart) +
@@ -74,9 +100,14 @@ function processInputOrTextArea(element) {
     // const newPosition = lastWordStart + shortcuts[lastWord].length;
 
     // // the line below dosent return anything so no need to assign it to anything
-    // element.setSelectionRange(newPosition, newPosition); 
+    // element.setSelectionRange(newPosition, newPosition);
     // THIS WHOLE CODE BLOCK ABOVE IS REPLACED BY THE FUNCTION CALL BELOW.
-    const { newText, newCursorPosition } = replaceLastWordInText(text, cursorPosition, original, shortcuts[sanitized]);
+    const { newText, newCursorPosition } = replaceLastWordInText(
+      text,
+      cursorPosition,
+      original,
+      shortcuts[sanitized]
+    );
     element.value = newText;
     element.setSelectionRange(newCursorPosition, newCursorPosition);
   }
@@ -87,21 +118,21 @@ function processInputOrTextArea(element) {
 // Process [contenteditable]
 function processContentEditable(element) {
   const selection = window.getSelection();
-  if(!selection.rangeCount) return;
+  if (!selection.rangeCount) return;
 
   const range = selection.getRangeAt(0); // the comment below contains everthing this line will give us that will be used further in code by doing "range.xyz", like done below for node and offset.
-//   Range {
-//   startContainer: #text "Hello, this is a test.",
-//   startOffset: 11,
-//   endContainer: #text "Hello, this is a test.",
-//   endOffset: 11,
-//   collapsed: true
-// }
+  //   Range {
+  //   startContainer: #text "Hello, this is a test.",
+  //   startOffset: 11,
+  //   endContainer: #text "Hello, this is a test.",
+  //   endOffset: 11,
+  //   collapsed: true
+  // }
   const node = range.startContainer; // For example, in <div contenteditable>hi|</div>, this would be the text node containing "hi".
   const offset = range.startOffset; // in short gives the index of caret
 
   // trying to stay in text node
-  if(node.nodeType !== Node.TEXT_NODE) return; // a check to verify the node being a text node, if not a text node, function stops.
+  if (node.nodeType !== Node.TEXT_NODE) return; // a check to verify the node being a text node, if not a text node, function stops.
 
   const text = node.textContent;
   const textBeforeCursor = text.substring(0, offset); // selects all the text before the caret position that is the offset.
@@ -131,9 +162,15 @@ function processContentEditable(element) {
   //   selection.removeAllRanges(); // removes the existing range i.e the previous one.
   //   selection.addRange(newRange); // adds this new range.
   // }
-  // THE CODE/FUNCTION BELOW HAS REPLACED WHAT HAS BEEN COMMENTED OUT ON TOP. 
-  if(shortcuts[sanitized]) { // changes were made lastWord -> sanitized
-    replaceLastWordInContentEditable(node, offset, original, shortcuts[sanitized]);
+  // THE CODE/FUNCTION BELOW HAS REPLACED WHAT HAS BEEN COMMENTED OUT ON TOP.
+  if (shortcuts[sanitized]) {
+    // changes were made lastWord -> sanitized
+    replaceLastWordInContentEditable(
+      node,
+      offset,
+      original,
+      shortcuts[sanitized]
+    );
   }
 }
 
@@ -143,26 +180,34 @@ function processContentEditable(element) {
 // currently we can handle elements that were loaded on the page with the initial load, but we cant handle inputs and editors added dynamically after the page load, in order to detect them we would require this.
 // example of SPAs - Notion, Gmail.
 
-function isTargetElement(el) {    // the basic use case of this function is that we will call this fucntion whenever we need to check if the node/element is textarea, input or contenteditable, and it will be returning true or false.
-  return (                        // this dosent work on its own, we use it just to check so we call it when we need and it answers accordingly.
-    el.tagName === "INPUT" ||
-    el.tagName === "TEXTAREA" ||
-    el.isContentEditable
+function isTargetElement(el) {
+  // the basic use case of this function is that we will call this fucntion whenever we need to check if the node/element is textarea, input or contenteditable, and it will be returning true or false.
+  return (
+    // this dosent work on its own, we use it just to check so we call it when we need and it answers accordingly.
+    el.tagName === "INPUT" || el.tagName === "TEXTAREA" || el.isContentEditable
   ); // basically to detect input, textarea and contenteditable like we did above
 } // this return is a boolean type, wil return true or false
 
-const observer = new MutationObserver((mutationsList) => { // so here this MutationObserver is an object that watches the DOM, and whenever the change is detected like an element is added, it triggers the callback function present inside mutationsList i.e. for list of changes that took place.
-  for(const mutation of mutationsList) {      // this means out of the list provided to it of the changes that is mutations here, take a single mutation, its written here like a enhanced for loop in java.
-    if(mutation.type === "childList") {      // we only want to cater the new elements added so we onky focus on the type "childList".
-      mutation.addedNodes.forEach((node) => {  // for every change/mutation taking place, there can be multiple elements/nodes added for each mutation so we need to sort out the ones we need.
-        if(!(node instanceof HTMLElement)) return;   // If the node is not an HTMLElement, we skip it (like text comments, etc.).
-        
-        if(isTargetElement(node)) {     // we call the function from top that we created to check. this if condition is checking the "parent node".
-          node.addEventListener("keydown", expandShortcuts);   // if it says true that means we found the element/node that concerns us, meaning we will cater it.
+const observer = new MutationObserver((mutationsList) => {
+  // so here this MutationObserver is an object that watches the DOM, and whenever the change is detected like an element is added, it triggers the callback function present inside mutationsList i.e. for list of changes that took place.
+  for (const mutation of mutationsList) {
+    // this means out of the list provided to it of the changes that is mutations here, take a single mutation, its written here like a enhanced for loop in java.
+    if (mutation.type === "childList") {
+      // we only want to cater the new elements added so we onky focus on the type "childList".
+      mutation.addedNodes.forEach((node) => {
+        // for every change/mutation taking place, there can be multiple elements/nodes added for each mutation so we need to sort out the ones we need.
+        if (!(node instanceof HTMLElement)) return; // If the node is not an HTMLElement, we skip it (like text comments, etc.).
+
+        if (isTargetElement(node)) {
+          // we call the function from top that we created to check. this if condition is checking the "parent node".
+          node.addEventListener("keydown", expandShortcuts); // if it says true that means we found the element/node that concerns us, meaning we will cater it.
         }
 
-        if(node.nodeType === Node.ELEMENT_NODE && node.querySelectorAll) {    // this if condition is for the children elements under the parent, for cases like if parent isnt a input, textarea or contenteditable but the children are.
-          const fields = node.querySelectorAll("input, textarea, [contenteditable='true']");    // querySelector is required in order to select those 3 things out of the element
+        if (node.nodeType === Node.ELEMENT_NODE && node.querySelectorAll) {
+          // this if condition is for the children elements under the parent, for cases like if parent isnt a input, textarea or contenteditable but the children are.
+          const fields = node.querySelectorAll(
+            "input, textarea, [contenteditable='true']"
+          ); // querySelector is required in order to select those 3 things out of the element
           fields.forEach((el) => {
             el.addEventListener("keydown", expandShortcuts);
           });
@@ -172,9 +217,10 @@ const observer = new MutationObserver((mutationsList) => { // so here this Mutat
   }
 });
 
-observer.observe(document.body, {   // here we are telling observer to keep watching the complete body of the document for changes.
-  childList: true,    // this tells the observer to watch for the nodes that are being added and removed in the given section, in this case the whole body.
-  subtree: true,      // this tells the observer to dive deeper meaning dont just watch document.body but also recursively go deep inside the nested child of the body.
+observer.observe(document.body, {
+  // here we are telling observer to keep watching the complete body of the document for changes.
+  childList: true, // this tells the observer to watch for the nodes that are being added and removed in the given section, in this case the whole body.
+  subtree: true, // this tells the observer to dive deeper meaning dont just watch document.body but also recursively go deep inside the nested child of the body.
 });
 // We’re telling the observer:
 // “Start watching the entire page for any new elements added anywhere inside it — not just on top-level body but deep down too.”
@@ -183,7 +229,7 @@ chrome.storage.onChanged.addListener((changes, namespace) => {
   // Listen for changes in Chrome storage
   if (namespace === "sync" && changes.shortcuts) {
     // If 'shortcuts' changed in the 'sync' storage area
-    shortcuts = changes.shortcuts.newValue || {};  // Update local shortcuts object
+    shortcuts = changes.shortcuts.newValue || {}; // Update local shortcuts object
     console.log("Shortcuts updated from storage:", shortcuts);
   }
 });
@@ -201,29 +247,32 @@ function getLastWord(textBeforeCursor) {
   // Now getLastWord() gives you both:
   // original → e.g., "yes,"
   // sanitized → e.g., "yes"
-} 
+}
 
 function replaceLastWordInText(text, cursorPosition, lastWord, expansion) {
   const textBeforeCursor = text.substring(0, cursorPosition);
   const lastWordStart = textBeforeCursor.length - lastWord.length;
 
-  const newText = text.substring(0, lastWordStart) + expansion + text.substring(cursorPosition);
+  const newText =
+    text.substring(0, lastWordStart) +
+    expansion +
+    text.substring(cursorPosition);
 
   const newCursorPosition = lastWordStart + expansion.length;
 
-  return {newText, newCursorPosition}; // here we are returning an object, basically we can return once only, so we return the object contaiing multiple values that helps us to get more out of the function.
+  return { newText, newCursorPosition }; // here we are returning an object, basically we can return once only, so we return the object contaiing multiple values that helps us to get more out of the function.
 }
 
 function replaceLastWordInContentEditable(node, offset, lastWord, expansion) {
   const selection = window.getSelection();
-  if(!selection) return;
+  if (!selection) return;
 
   const text = node.textContent;
   const textBeforeCursor = text.substring(0, offset);
   const lastWordStart = textBeforeCursor.length - lastWord.length; // tip for last word start - always take it according to textbeforecursor, meaning in accordance to the cursor.
-  
 
-  const newText = text.substring(0, lastWordStart) + expansion + text.substring(offset);
+  const newText =
+    text.substring(0, lastWordStart) + expansion + text.substring(offset);
 
   node.textContent = newText;
 
@@ -257,10 +306,10 @@ function sanitize(textBeforeCursor) {
   let lastWord = words[words.length - 1];
 
   // Base Case.
-  if(!/[.,!?;:]$/.test(lastWord)) {
+  if (!/[.,!?;:]$/.test(lastWord)) {
     return lastWord;
   }
-  
+
   // removing trailing punctuation.
   lastWord = lastWord.slice(0, -1); // here we have updated last word by removing punctuation from the last.  // In JavaScript, when you use .slice(0, -1) on a string, it means: 0 is the starting index (the very beginning of the string). -1 is a special value: it means "up to, but not including, the last character."
   words[words.length - 1] = lastWord;
@@ -269,8 +318,9 @@ function sanitize(textBeforeCursor) {
   return sanitize(words.join(" ")); // It takes the array of words and joins them back into a single string, inserting a space (" ") between each word.
 }
 
-chrome.storage.onChanged.addListener((change, namespace) => { // to see if user enables/disables extension while being on the page itself.
-  if(namespace === "sync" && change.snapTextEnabled) {
+chrome.storage.onChanged.addListener((change, namespace) => {
+  // to see if user enables/disables extension while being on the page itself.
+  if (namespace === "sync" && change.snapTextEnabled) {
     snapTextEnabled = change.snapTextEnabled.newValue;
     console.log("Toggle updated:", snapTextEnabled);
   }
